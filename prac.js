520104f523773047a254578a6db1f56d1441400e
haleemaraza <152488926+haleemaraza@users.noreply.github.com>
2023-12-11 13:33:46 +0500

    Add files via upload

---
+++ prac.js
@@ -0,0 +1,58 @@
+//  let  l = "haleema";
+//  let w ="18";
+//there are two kinds of pattner to console
+// console.log (`my name is ${l} , my age is ${w}`);
+// is console may letters par "" deya ha  
+// console.log("my name is "+l+" , my age is "+w);
+//  let powder = "lal mirch ";
+//is may "" we put this to print something new like (console.log("hello") this )
+// console.log(powder);
+//  powder = "haldi";
+// console.log(powder);
+//string method
+//   let _fullName = "je chang wook";
+//_fullName = "le jong suk";
+//_fullName = "hyeri";
+//console.log (_fullName);
+//const (we cant replace the const value)
+// const _h = "nothing ";
+// _h = "nothing";
+// console.log(_h)  ;
+//adding two value 
+// let firstName = "khadija";
+// let lastName = "butt"  ;
+// console.log (firstName + " " + lastName)
+//undefined value 
+// let userName ; 
+// console.log (userName);
+//userName = "Do ki ";
+//we put console again   
+// console.log(userName);
+//here we making briyani 
+//   qty = ytq
+// let ytq = 8; 
+//  console.log (ytq - 4 ) ; 
+// console.log (ytq + 4)
+//qty multiply by each container price;
+// const _ytq  =    4
+// console.log (_ytq * 12000)    
+// we have to distribute in 3 row
+// let _ytq = 4
+//  console.log (_ytq / 3 )
+// let str1 = "1"
+// let str2 = "2"
+// console.log (str1 + str2)
+// let n1 = 2 
+// let n2 = 3 
+// console.log (n1 ** n2 )
+//maths 
+// let age = 28
+// age += -10
+// console.log (age)    
+// let n = 5 
+// console.log (n)
+// n += 5
+// console.log (n)
+var age = 28;
+// console.log (age == 28 ); (age === 28 ) (same answer aayaga )
+console.log(age != 28);
---
+++ prac.ts
@@ -0,0 +1,110 @@
+// CLASS 1
+
+console.log("hello world");
+
+
+// type of variable
+// let
+let fullName = "Arfa";
+console.log(fullName);
+// var
+var Name = "Aylin";
+console.log(fullName);
+// const
+const MyName = "Alif";
+console.log(fullName);
+
+
+
+// CLASS 2
+
+
+// making variables 
+ let  l = "haleema";
+ let h ="18";
+// now print your variables
+ console.log (`my name is ${l} , my age is ${h}`);
+ console.log("my name is "+l+" , my age is "+h);
+
+
+// we can also change variable
+ let powder = "lal mirch ";
+console.log(powder);
+ powder = "haldi";
+console.log(powder);
+
+
+//  string method
+  let _fullName = "je chang wook";
+_fullName = "le jong suk";
+_fullName = "hyeri";
+console.log (_fullName);
+
+
+//   const 
+const js = "nothing ";
+//  js= "dollar"; (we cant replace the const value)
+console.log(js)  ;
+
+
+//   adding two value 
+let firstName = "khadija";
+let lastName = "butt"  ;
+console.log (firstName + " " + lastName);
+
+
+//   undefined value 
+let userName ; 
+console.log (userName);
+// defined value
+userName = "Do ki ";
+console.log(userName);
+  
+
+//   here we making briyani  
+ let qty = 8; 
+ console.log (qty - 4 ) ; 
+console.log (qty + 4);
+// qty multiply by each container price;
+ let _qty  =    4;
+console.log (_qty * 12000);
+//  we have to distribute in 3 row
+ let _ytq = 4;
+console.log (_ytq / 3 );
+
+
+// Arthimetic operators
+ let str1 = "1";
+ let str2 = "2";
+ console.log (str1 + str2);
+let n1 = 2 ;
+ let n2 = 3 ;
+console.log (n1 ** n2 );
+
+
+// assignment operator
+let _age = 28;
+_age += -10;
+console.log (_age); 
+let n = 5 ;
+console.log (n);
+n += 5;
+console.log (n);
+
+
+// Modulus
+let _n1 = 10;
+let _n2 = 3;
+console.log(_n1 % _n2); 
+
+
+// operator
+let age = 28 ;
+console.log (age == 28 ); 
+console.log (age != 28 ) ;
+console.log (age > 10);
+console.log (age >=28 );
+console.log (age <= 28);
+let  w = 5 
+console.log (n > 8 );
+console.log (n < 5 );
